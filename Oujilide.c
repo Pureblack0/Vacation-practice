fbe1f1c94b10e2eb69adcbf13a99bf9369f3cf80
Pureblack0 <522868685@qq.com>
2020-07-13 20:53:39 +0800

    First

---
+++ Oujilide.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main(){
+    int a;                    
+    int b;                    
+    int r=1;                  // 余数
+    printf("输入两个数字：");
+    scanf("%d %d",&a,&b);
+    while(r!=0){             
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    printf("最大公约数为：%d\n",a); 
+    system("pause");       
+    return 0;
+}
+
