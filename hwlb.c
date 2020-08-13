aa2e7b91c0d5395a756f497bcc9ff9b7068952de
Pureblack0 <522868685@qq.com>
2020-08-13 19:19:41 +0800

    回文链表

---
+++ hwlb.c
@@ -0,0 +1,51 @@
+/*请判断一个链表是否为回文链表。
+示例 1:
+输入: 1->2
+输出: false
+示例 2:
+输入: 1->2->2->1
+输出: true*/
+bool isPalindrome(struct ListNode* head){
+    // 特殊情况的排除
+    if(head == NULL || head->next == NULL)
+        return true;
+    if(head->next->next == NULL){
+        if(head->val == head->next->val)
+            return true;
+        else
+            return false;
+    }
+
+    struct ListNode *fastp, *slowp;
+    fastp = head->next->next;
+    slowp = head->next;
+
+    // 快慢指针找到尾部及中部位置
+    while(fastp && fastp->next != NULL){
+        fastp = fastp->next->next;
+        slowp = slowp->next;
+    }
+
+    // 翻转中部前链表序列
+    struct ListNode *prep, *nextp;
+    prep = nextp = NULL;
+    while(head != slowp){
+        nextp = head->next;
+        head->next = prep;
+        prep = head;
+        head = nextp;
+    }
+
+    // 若结点个数为奇数，则舍弃中间结点
+    if(fastp != NULL && fastp->next == NULL)
+        slowp = slowp->next;
+
+    // 回文匹配比较
+    while(prep != NULL){
+        if(prep->val != slowp->val)
+            return false;
+        prep = prep->next;
+        slowp = slowp->next;
+    }
+    return true;
+}
