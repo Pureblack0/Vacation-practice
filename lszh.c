bed3a98966fd3621d047bd26148ba1bccf278106
Pureblack0 <522868685@qq.com>
2020-08-23 19:48:37 +0800

    两数之和（输入有序数组）

---
+++ lszh.c
@@ -0,0 +1,36 @@
+/*给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+说明:
+返回的下标值（index1 和 index2）不是从零开始的。
+你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+示例:
+输入: numbers = [2, 7, 11, 15], target = 9
+输出: [1,2]
+解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+来源：力扣（LeetCode）*/
+/*方法：二分查找
+在数组中找到两个数，使得它们的和等于目标值，可以首先固定第一个数，然后寻找第二个数，第二个数等于目标值减去第一个数的差。
+利用数组的有序性质，可以通过二分查找的方法寻找第二个数。为了避免重复寻找，在寻找第二个数时，只在第一个数的右侧寻找。*/
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
+    int* ret = (int*)malloc(sizeof(int) * 2);
+    *returnSize = 2;
+
+    for (int i = 0; i < numbersSize; ++i) {
+        int low = i + 1, high = numbersSize - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            if (numbers[mid] == target - numbers[i]) {
+                ret[0] = i + 1, ret[1] = mid + 1;
+                return ret;
+            } else if (numbers[mid] > target - numbers[i]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+    }
+    ret[0] = -1, ret[1] = -1;
+    return ret;
+}
+
+
